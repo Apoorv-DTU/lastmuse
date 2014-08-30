ffb5c3fc56c10a90ade090242d0cee33cabcceb2
Apoorv <ap.goel98@gmail.com>
2014-08-30 15:14:52 +0300

    Moved to python3 and followed PEP8

--- bin/cli.py
+++ bin/cli.py
@@ -1,5 +1,9 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
+#!/usr/bin/python3
+
+from __future__ import print_function
+from __future__ import unicode_literals
+
+# Set the directory to the parent
 import os
 import sys
 script_dir = os.getcwd()
@@ -12,16 +16,15 @@ tracks = last.fetch_tracks()
 if len(sys.argv) < 2:
 
     for i in range(len(tracks)):
-        print "[%d] %s" % (i+1, tracks[i])
-
+        print("[{:d}] {:s}".format(i+1, tracks[i]))
 else:
     index = int(sys.argv[1]) - 1
-    
-    #check if the argument is in bounds
-    if index < 0: 
+
+    # Check if the argument is in bounds
+    if index < 0:
         index = 1
     elif index > 19:
         index = 19
 
     url = last.vimeo_url_from_track(tracks[index])
-    print "[%d] %s: %s" % (index+1, tracks[index], url)
+    print("[{:d}] {:s}: {:s}".format(index+1, tracks[index], url))
--- lastmuse/last.py
+++ lastmuse/last.py
@@ -1,27 +1,36 @@
-#!/usr/bin/env python
-# -*- coding: UTF-8 -*-
+#!/usr/bin/env python3
+
+from __future__ import print_function
+from __future__ import unicode_literals
 from random import random
+
 import requests
 from lxml import html
 
+
 def _gen_headers():
-    agents = [("Mozilla/5.0 (compatible; MSIE 10.6; Windows NT 6.1; Trident/5.0;" 
-                   "InfoPath.2; SLCC1; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729; "
-                   ".NET CLR 2.0.50727) 3gpp-gba UNTRUSTED/1.0"),           
+    agents = ["Mozilla/5.0 (compatible; MSIE 10.6; Windows NT 6.1; "
+              "Trident/5.0; InfoPath.2; SLCC1; "
+              ".NET CLR 3.0.4506.2152; .NET CLR 3.5.30729; "
+              ".NET CLR 2.0.50727) 3gpp-gba UNTRUSTED/1.0",
 
-                   ("Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 "
-                    "(KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36"),
+              "Mozilla/5.0 (Windows NT 6.3; Win64; x64) "
+              "AppleWebKit/537.36 (KHTML, like Gecko) "
+              "Chrome/37.0.2049.0 Safari/537.36",
 
-                   "Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0",
+              "Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101"
+              " Firefox/31.0",
 
-                   ("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/537.13"
-                    "+ (KHTML, like Gecko) Version/5.1.7 Safari/534.57.2")]
+              "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) "
+              "AppleWebKit/537.13+ (KHTML, like Gecko) "
+              "Version/5.1.7 Safari/534.57.2"]
 
     index = int(random() * 100000 % 4)
     return {'User-Agent': agents[index]}
 
+
 def fetch_tracks():
-    url = "http://www.last.fm/charts/tracks/top/place/all" 
+    url = "http://www.last.fm/charts/tracks/top/place/all"
     fm_r = requests.get(url)
     fm_tree = html.fromstring(fm_r.text)
 
@@ -29,46 +38,50 @@ def fetch_tracks():
     for srl in range(1, 21):
         if srl is 1:
             track_xpath = ("/html/body/div[1]/div/div[6]/div[1]/div[1]/div/ol/"
-                            "li[1]/div/div/a[1]/h4/span[2]/text()") 
+                           "li[1]/div/div/a[1]/h4/span[2]/text()")
         else:
             track_xpath = ("/html/body/div[1]/div/div[6]/div[1]/div[1]/div/ol/"
-                            "li[" + str(srl) + "]/div/div/a/h4/span[2]/text()")
-                           
+                           "li[" + str(srl) + "]/div/div/a/h4/span[2]/text()")
+
         song = fm_tree.xpath(track_xpath)[0]
         tracklist.append(song)
     return tracklist
 
+
 def vimeo_url_from_track(track):
-    url = track.replace(u' \u2013 ', ' ')
+    url = track.replace(' \u2013 ', ' ')
     url = url.replace('+', '%2B')
     url = url.replace('?', '%3F')
     url = url.replace(' ', '+')
     url = url.replace('!', '')
     url = url.lower()
-    
+
     head = _gen_headers()
 
     search_req = requests.get("http://vimeo.com/search?q=" + url, headers=head)
     if search_req.status_code != 200:
-        print "Error %d: %s from %s" % (search_req.status_code, search_req.url, head['User-Agent'])
-        
+        print("Error {:d}: {:s} from {:s}".format(search_req.status_code,
+                                                  search_req.url,
+                                                  head['User-Agent']))
+
     res_tree = html.fromstring(search_req.text)
     xpath = ("/html/body/div[1]/div[2]/div[2]/div/div[1]/div[1]/"
              "div[3]/ol/li[2]/a/@href")
-    
+
     url = "http://vimeo.com" + res_tree.xpath(xpath)[0]
 
     vid_req = requests.get(url, headers=head)
     vid_tree = html.fromstring(vid_req.text)
 
     vid_xpath = ("/html/body/div[1]/div[2]/div[2]/div/div[1]/"
-                    "div[1]/div/div/@data-config-url")
+                 "div[1]/div/div/@data-config-url")
 
     vid_url = vid_tree.xpath(vid_xpath)[0]
     json_r = requests.get(vid_url, headers=head)
     j = json_r.json()
+
     try:
-        raw_vid = j[u'request'][u'files'][u'h264'][u'hd'][u'url']
+        raw_vid = j['request']['files']['h264']['hd']['url']
     except KeyError:
-        raw_vid = j[u'request'][u'files'][u'h264'][u'sd'][u'url'] 
+        raw_vid = j['request']['files']['h264']['sd']['url']
     return raw_vid
